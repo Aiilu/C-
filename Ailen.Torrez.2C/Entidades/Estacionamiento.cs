6285d08bb51f3bb21406165506c24feba8a914c6
Aiilu <ailentorrez26@gmail.com>
2020-10-04 15:34:20 -0300

    No se que pasa o.0

---
+++ Ailen.Torrez.2C/Entidades/Estacionamiento.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Estacionamiento
+    {
+        private int espacioDisponible;
+        private string nombre;
+        private List<Vehiculo> vehiculos;
+
+        private Estacionamiento()
+        {
+            this.vehiculos = new List<Vehiculo>();
+        }
+
+        public Estacionamiento(string nombre, int espacioDisponible) :this()
+        {
+            this.nombre = nombre;
+            this.espacioDisponible = espacioDisponible;
+        }
+
+        public static explicit operator string(Estacionamiento e)
+        {
+            StringBuilder datos = new StringBuilder();
+
+            datos.AppendLine($" Nombre: {e.nombre}");
+            datos.AppendLine($" Espacio disponible: {e.espacioDisponible}");
+
+            foreach(Vehiculo v in e.vehiculos)
+            {
+                datos.AppendLine(v.ToString());
+            }
+
+            return datos.ToString();
+        }
+
+        public static bool operator ==(Estacionamiento e, Vehiculo v)
+        {
+            foreach(Vehiculo vh in e.vehiculos)
+            {
+                if(vh == v)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static bool operator !=(Estacionamiento e, Vehiculo v)
+        {
+            return !(e == v);
+        }
+
+        public static Estacionamiento operator +(Estacionamiento e, Vehiculo v)
+        {
+            if(e.espacioDisponible > e.vehiculos.Count)
+            {
+                if(e != v && !string.IsNullOrWhiteSpace(v.Patente))
+                {
+                    e.vehiculos.Add(v);
+                }
+            }
+
+            return e;
+        }
+
+        public static string operator -(Estacionamiento e, Vehiculo v)
+        {
+            if(e == v)
+            {
+                e.vehiculos.Remove(v);
+                return v.ImprimirTicket();
+            }
+
+            return "El vehiculo no es parte del estacionamiento";
+        }
+    }
+}
---
+++ Ailen.Torrez.2C/VistaForm/FrmPickup.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace VistaForm
+{
+    partial class FrmPickup
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPatente = new System.Windows.Forms.Label();
+            this.txtPatentee = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtModelo = new System.Windows.Forms.TextBox();
+            this.btnCrear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblPatente
+            // 
+            this.lblPatente.AutoSize = true;
+            this.lblPatente.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPatente.Location = new System.Drawing.Point(63, 25);
+            this.lblPatente.Name = "lblPatente";
+            this.lblPatente.Size = new System.Drawing.Size(72, 20);
+            this.lblPatente.TabIndex = 0;
+            this.lblPatente.Text = "Patente";
+            // 
+            // txtPatentee
+            // 
+            this.txtPatentee.Location = new System.Drawing.Point(30, 48);
+            this.txtPatentee.Name = "txtPatentee";
+            this.txtPatentee.Size = new System.Drawing.Size(153, 20);
+            this.txtPatentee.TabIndex = 1;
+            this.txtPatentee.Leave += new System.EventHandler(this.txtPatente_OnLeave);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(68, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Modelo";
+            // 
+            // txtModelo
+            // 
+            this.txtModelo.Location = new System.Drawing.Point(30, 124);
+            this.txtModelo.Name = "txtModelo";
+            this.txtModelo.Size = new System.Drawing.Size(153, 20);
+            this.txtModelo.TabIndex = 3;
+            this.txtModelo.Leave += new System.EventHandler(this.txtModelo_OnLeave);
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.Enabled = false;
+            this.btnCrear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCrear.Location = new System.Drawing.Point(30, 197);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(153, 60);
+            this.btnCrear.TabIndex = 4;
+            this.btnCrear.Text = "Crear";
+            this.btnCrear.UseVisualStyleBackColor = true;
+            this.btnCrear.Click += new System.EventHandler(this.btnCrear_Click);
+            // 
+            // FrmPickup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(215, 286);
+            this.Controls.Add(this.btnCrear);
+            this.Controls.Add(this.txtModelo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPatentee);
+            this.Controls.Add(this.lblPatente);
+            this.Name = "FrmPickup";
+            this.Text = "Crear PickUp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPatente;
+        private System.Windows.Forms.TextBox txtPatentee;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtModelo;
+        private System.Windows.Forms.Button btnCrear;
+    }
+}
+
