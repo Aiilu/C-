433ee6154f3b6f08c92188be9e3e29142f2699d9
Aiilu <ailentorrez26@gmail.com>
2020-10-30 16:09:48 -0300

    Ejercicios Interfaz

---
+++ Interfaces/Ejercicio52/Entidades/Boligrafo.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Boligrafo :IAcciones
+    {
+        private ConsoleColor colorTinta;
+        private float tinta;
+
+        public Boligrafo(int unidades, ConsoleColor color)
+        {
+            this.tinta = unidades;
+            this.colorTinta = color;
+        }
+
+        public ConsoleColor Color
+        {
+            get
+            {
+                return this.colorTinta;
+            }
+            set
+            {
+                this.colorTinta = value;
+            }
+
+        }
+
+        public float UnidadesDeEscritura
+        {
+            get
+            {
+                return this.tinta;
+            }
+            set
+            {
+                this.tinta = value;
+            }
+
+        }
+
+        public EscrituraWrapper Escribir(string texto)
+        {
+            for (int i = 0; i < texto.Length; i++)
+            {
+                this.UnidadesDeEscritura -= 0.3f;
+            }
+
+            return new EscrituraWrapper(texto, this.Color);
+        }
+
+        public bool Recargar(int unidades)
+        {
+            for(int i=0; i<unidades; i++)
+            {
+                this.UnidadesDeEscritura += 1;
+            }
+
+            return true;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder datos = new StringBuilder();
+
+            datos.AppendLine(" Hola, soy un Boligrafo");
+            datos.AppendLine($" Color: {this.Color}");
+            datos.AppendLine($" Nivel de Tinta: {this.UnidadesDeEscritura}");
+
+            return datos.ToString();
+        }
+    }
+}
---
+++ Interfaces/Ejercicio52/Entidades/Lapiz.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Lapiz : IAcciones
+    {
+        private float tamanioMina;
+
+        public Lapiz(int unidades)
+        {
+            this.tamanioMina = unidades;
+        }
+
+        /// <summary>
+        /// ESTO NO PUEDE SER EXPLICITO
+        /// </summary>
+        public ConsoleColor Color 
+        {
+            get
+            {
+                return ConsoleColor.Gray;
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+            
+        }
+
+        public float UnidadesDeEscritura
+        {
+            get
+            {
+                return this.tamanioMina;
+            }
+            set
+            {
+                this.tamanioMina = value;
+            }
+
+        }
+
+        public override string ToString()
+        {
+            StringBuilder datos = new StringBuilder();
+
+            datos.AppendLine(" Hola, soy un Lapiz");
+            datos.AppendLine($" Color: {this.Color}");
+            datos.AppendLine($" Nivel de Tinta: {this.UnidadesDeEscritura}");
+
+            return datos.ToString();
+        }
+
+        EscrituraWrapper IAcciones.Escribir(string texto)
+        {
+            for(int i=0; i<texto.Length; i++)
+            {
+                this.UnidadesDeEscritura -= 0.1f;
+            }
+
+            return new EscrituraWrapper(texto, this.Color);
+        }
+
+        bool IAcciones.Recargar(int unidades)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
