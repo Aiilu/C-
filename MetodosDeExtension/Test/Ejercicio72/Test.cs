c9be87d4247dbbd15a013c77a09cc39ef9d26d56
Aiilu <ailentorrez26@gmail.com>
2020-11-09 12:04:29 -0300

    usame mas

--- MetodosDeExtension/Test/Ejercicio72/Test.cs
+++ MetodosDeExtension/Test/Ejercicio72/Test.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entidades;
 
 namespace Ejercicio72
 {
-    class Program
+    public class Test
     {
         static void Main(string[] args)
         {
+            long caca = 333;
+
+            Console.WriteLine(caca.CantidadDeDigitos());
+
+            Console.ReadKey();
         }
     }
 }
---
+++ MetodosDeExtension/Test/Entidades/ExtLong.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public static class ExtLong
+    {
+        public static int CantidadDeDigitos(this long var)
+        {
+            return (var.ToString()).Length;
+        }
+    }
+}
